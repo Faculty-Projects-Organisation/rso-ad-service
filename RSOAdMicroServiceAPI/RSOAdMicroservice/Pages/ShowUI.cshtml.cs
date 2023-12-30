9ed88f726c072a051a2dc8f83fa72b5c77ba0f9f
Aleksander Kovač <aleksander.kovac97@hotmail.com>
2023-12-30 21:43:05 +0100

    Pozabil dodati ShowUI.cshtml

---
+++ RSOAdMicroServiceAPI/RSOAdMicroservice/Pages/ShowUI.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace RSOAdMicroservice.Pages
+{
+    public class ShowUIModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
